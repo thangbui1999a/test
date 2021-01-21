7c18be58760bccf608dbcb1428498de5654079b0
Thang Bui <thangbui@kms.com.vn>
2021-01-21 15:30:34 +0700

    update

--- mytest.js
+++ mytest.js
@@ -3,5 +3,16 @@
 
 console.log('testing...')
 
-console.log('tested ok2!')
+function sleep(millisecs){
+	return new Promise((resolve)=>{
+		setTimeout(()=>{
+			
+			console.log('tested ok2!')
+			resolve('ok')
+		}, 
+		millisecs)
+	})
+}
+sleep(10000)
+
 
